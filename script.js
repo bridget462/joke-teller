482da60103cbf1ce84989ee46e9c3c7fe2a08f92
bridget462 <bridget.dev.create@gmail.com>
2020-08-25 20:39:16 -0700

    Joke button disabled while speaking

--- script.js
+++ script.js
@@ -105,6 +105,11 @@ const VoiceRSS = {
   },
 };
 
+// Disable/Enable Button
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+
 // Passing Joke to VoiceRSS API
 function tellMe(joke) {
   VoiceRSS.speech({
@@ -133,10 +138,15 @@ async function getJokes() {
     } else {
       joke = data.joke;
     }
+    // Text to Speech
     tellMe(joke);
+    // Disable Button
+    toggleButton();
   } catch (error) {
     console.log("Whoops!", error);
   }
 }
 
-getJokes();
+// Event Listeners
+button.addEventListener("click", getJokes);
+audioElement.addEventListener("ended", toggleButton);
